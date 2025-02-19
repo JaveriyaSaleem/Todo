10a47a48ca84be8d4dbec31d01083d46b91bdf38
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-02-19 10:52:50 +0500

    added

--- app.mjs
+++ app.mjs
@@ -1,10 +1,30 @@
 import express from "express"
+import path from "path"
 const app = express()
-const port = process.env.Port||3000
+const port = process.env.PORT ||  1000
+
+const __dirname = path.resolve()
+
+app.use('/', express.static(path.join(__dirname, './React/dist')))
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Javeriya')
+  res.send("get  chlrha ha")
 })
+
+app.get('/weather', (req, res) => {
+  res.json({ user: 'tobi' })
+})
+app.get('/weather/:cityName', (req, res) => {
+  res.send(req.params)
+})
+app.get('/weather/:cityName/:side', (req, res) => {
+  res.send(req.params)
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
